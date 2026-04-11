3fe1d98a4a71f3e726281166a0ae77686762dd49
RishavShrestha1 <weeesdom1@gmail.com>
2026-04-11 17:45:25 +0545

    minor problem

--- initSDL.h
+++ initSDL.h
@@ -26,7 +26,7 @@ bool init() {
     }
 
     else{
-        shellWindow = SDL_CreateWindow("shell", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGTH, SDL_WINDOW_SHOWN);
+        shellWindow = SDL_CreateWindow("shell", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         shellRenderer = SDL_CreateRenderer(shellWindow, -1, SDL_RENDERER_ACCELERATED);
         if(shellWindow == NULL || shellRenderer == NULL){
             std::cerr << "Unable to create window and/or renderer! SDL_ERROR : "<<SDL_GetError()<<std::endl;
